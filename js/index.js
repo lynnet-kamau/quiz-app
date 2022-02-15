1b7490c804896092e76bcf7760defed13b4eebd5
lynnet-kamau <linetwanjiku033@gmail.com>
2022-02-15 10:25:35 +0300

    new

--- js/index.js
+++ js/index.js
@@ -1,167 +1,153 @@
-// const restartBtn = document.getElementById("restart");
-// const prevBtn = document.getElementById("prev");
-// const nextBtn = document.getElementById("next");
-// const submitBtn = document.getElementById("submit");
-// const trueBtn = document.getElementById("True");
-// const falseBtn = document.getElementById("False");
-// const userScore = document.getElementById("user-score");
-// const totalScore = document.getElementById("total-score");
-// const questionText = document.getElementById("question-text")
+const restartBtn = document.getElementById("restart");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const submitBtn = document.getElementById("submit");
+const trueBtn = document.getElementById("True");
+const falseBtn = document.getElementById("False");
+const userScore = document.getElementById("user-score");
+const totalScore = document.getElementById("total-score");
+const questionText = document.getElementById("question-text")
 
-// let currentQuestion = 0;
-// let score = 0;
+let currentQuestion = 0;
+let score = 0;
 
-// let questions = [{
-//         question: "is javascript the best programming language?",
-//         answers: [
-//             { option: "For Sure!", answer: true },
-//             { option: "No, not at all.", answer: false },
-//         ]
-//     },
-//     {
-//         question: "What is better if you wish to achieve success?",
-//         answers: [
-//             { option: "Hard Work", answer: false },
-//             { option: "Smart Work", answer: true },
-//         ]
-//     },
-//     {
-//         question: "Complete the phrase: Time and ___ wait for none.",
-//         answers: [
-//             { option: "Batman", answer: false },
-//             { option: "Tide", answer: true },
-//         ]
-//     }
-// ]
-// restartBtn.addEventListener("click", restart);
-// prevBtn.addEventListener("click", prev);
-// nextBtn.addEventListener("click", next);
-// submitBtn.addEventListener("click", submit);
+let questions = [{
+        question: "is javascript the best programming language?",
+        answers: [
+            { option: "For Sure!", answer: true },
+            { option: "No, not at all.", answer: false },
+        ]
+    },
+    {
+        question: "What is better if you wish to achieve success?",
+        answers: [
+            { option: "Hard Work", answer: false },
+            { option: "Smart Work", answer: true },
+        ]
+    },
+    {
+        question: "Complete the phrase: Time and ___ wait for none.",
+        answers: [
+            { option: "Batman", answer: false },
+            { option: "Tide", answer: true },
+        ]
+    }
+]
+restartBtn.addEventListener("click", restart);
+prevBtn.addEventListener("click", prev);
+nextBtn.addEventListener("click", next);
+submitBtn.addEventListener("click", submit);
 
-// function beginQuiz() {
-//     currentQuestion = 0;
-//     totalScore.innerHTML = questions.length;
-//     questionText.innerHTML = questions[currentQuestion].question;
-//     trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
-//     trueBtn.onclick = () => {
-//         if (questions[currentQuestion].answers[0].answer) {
-//             if (score < 3) {
-//                 score++;
-//             }
-//         }
-//         userScore.innerHTML = score;
-//         if (currentQuestion < 2) {
-//             next();
-//         }
-//     }
+function beginQuiz() {
+    currentQuestion = 0;
+    totalScore.innerHTML = questions.length;
+    questionText.innerHTML = questions[currentQuestion].question;
+    trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
+    trueBtn.onclick = () => {
+        if (questions[currentQuestion].answers[0].answer) {
+            if (score < 3) {
+                score++;
+            }
+        }
+        userScore.innerHTML = score;
+        if (currentQuestion < 2) {
+            next();
+        }
+    }
 
-//     falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
-//     falseBtn.onclick = () => {
-//         if (questions[currentQuestion].answers[1].answer) {
-//             if (score < 3) {
-//                 score++;
-//             }
-//         }
-//         userScore.innerHTML = score;
-//         if (currentQuestion < 2) {
-//             next();
-//         }
-//     }
+    falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
+    falseBtn.onclick = () => {
+        if (questions[currentQuestion].answers[1].answer) {
+            if (score < 3) {
+                score++;
+            }
+        }
+        userScore.innerHTML = score;
+        if (currentQuestion < 2) {
+            next();
+        }
+    }
 
-//     prevBtn.classList.add("hide");
-// }
+    prevBtn.classList.add("hide");
+}
 
-// beginQuiz();
+beginQuiz();
 
-// function restart() {
-//     currentQuestion = 0;
-//     prevBtn.classList.remove("hide");
-//     nextBtn.classList.remove("hide");
-//     submitBtn.classList.remove("hide");
-//     trueBtn.classList.remove("hide");
-//     falseBtn.classList.remove("hide");
-//     score = 0;
-//     userScore.innerHTML = score;
-//     beginQuiz();
-// }
+function restart() {
+    currentQuestion = 0;
+    prevBtn.classList.remove("hide");
+    nextBtn.classList.remove("hide");
+    submitBtn.classList.remove("hide");
+    trueBtn.classList.remove("hide");
+    falseBtn.classList.remove("hide");
+    score = 0;
+    userScore.innerHTML = score;
+    beginQuiz();
+}
 
-// function next() {
-//     currentQuestion++;
-//     if (currentQuestion >= 2) {
-//         nextBtn.classList.add("hide");
-//         prevBtn.classList.remove("hide");
-//     }
-//     questionText.innerHTML = questions[currentQuestion].question;
-//     trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
-//     trueBtn.onclick = () => {
-//         if (questions[currentQuestion].answers[0].answer) {
-//             if (score < 3) {
-//                 score++;
-//             }
-//         }
-//         userScore.innerHTML = score;
-//         if (currentQuestion < 2) {
-//             next();
-//         }
-//     }
+function next() {
+    currentQuestion++;
+    if (currentQuestion >= 2) {
+        nextBtn.classList.add("hide");
+        prevBtn.classList.remove("hide");
+    }
+    questionText.innerHTML = questions[currentQuestion].question;
+    trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
+    trueBtn.onclick = () => {
+        if (questions[currentQuestion].answers[0].answer) {
+            if (score < 3) {
+                score++;
+            }
+        }
+        userScore.innerHTML = score;
+        if (currentQuestion < 2) {
+            next();
+        }
+    }
 
-//     falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
-//     falseBtn.onclick = () => {
-//         if (questions[currentQuestion].answers[1].answer) {
-//             if (score < 3) {
-//                 score++;
-//             }
-//         }
-//         userScore.innerHTML = score;
-//         if (currentQuestion < 2) {
-//             next();
-//         }
-//     }
+    falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
+    falseBtn.onclick = () => {
+        if (questions[currentQuestion].answers[1].answer) {
+            if (score < 3) {
+                score++;
+            }
+        }
+        userScore.innerHTML = score;
+        if (currentQuestion < 2) {
+            next();
+        }
+    }
 
-//     prevBtn.classList.remove("hide");
-// }
+    prevBtn.classList.remove("hide");
+}
 
-// function prev() {
-//     currentQuestion--;
-//     if (currentQuestion <= 0) {
-//         nextBtn.classList.remove("hide");
-//         prevBtn.classList.add("hide");
-//     }
-//     questionText.innerHTML = questions[currentQuestion].question;
-//     trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
-//     trueBtn.onclick = () => {
-//         if (questions[currentQuestion].answers[0].answer) {
-//             if (score < 3) {
-//                 score++;
-//             }
-//         }
-//         userScore.innerHTML = score;
-//         if (currentQuestion < 2) {
-//             next();
-//         }
-//     }
+function prev() {
+    currentQuestion--;
+    if (currentQuestion <= 0) {
+        nextBtn.classList.remove("hide");
+        prevBtn.classList.add("hide");
+    }
+    questionText.innerHTML = questions[currentQuestion].question;
+    trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
+    trueBtn.onclick = () => {
+        if (questions[currentQuestion].answers[0].answer) {
+            if (score < 3) {
+                score++;
+            }
+        }
+        userScore.innerHTML = score;
+        if (currentQuestion < 2) {
+            next();
+        }
+    }
 
-//     falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
-//     falseBtn.onclick = () => {
-//         if (questions[currentQuestion].answers[1].answer) {
-//             if (score < 3) {
-//                 score++;
-//             }
-//         }
-//         userScore.innerHTML = score;
-//         if (currentQuestion < 2) {
-//             next();
-//         }
-//     }
 
-//     nextBtn.classList.remove("hide");
-// }
 
-// function submit() {
-//     prevBtn.classList.add("hide");
-//     nextBtn.classList.add("hide");
-//     submitBtn.classList.add("hide");
-//     trueBtn.classList.add("hide");
-//     falseBtn.classList.add("hide");
-//     questionText.innerHTML = "Congratulations on submitting the Quiz!"
-// }
+    function submit() {
+        prevBtn.classList.add("hide");
+        nextBtn.classList.add("hide");
+        submitBtn.classList.add("hide");
+        trueBtn.classList.add("hide");
+        falseBtn.classList.add("hide");
+        questionText.innerHTML = "Congratulations on submitting the Quiz!"
+    }
